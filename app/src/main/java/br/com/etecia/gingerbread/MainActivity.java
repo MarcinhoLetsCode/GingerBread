f241fc0f03e8e5393f14931b6201123b61225e92
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-09-30 09:01:35 -0300

    Drawer Menu

--- app/src/main/java/br/com/etecia/gingerbread/MainActivity.java
+++ app/src/main/java/br/com/etecia/gingerbread/MainActivity.java
@@ -1,30 +1,40 @@
 package br.com.etecia.gingerbread;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     MaterialToolbar materialToolbar;
     BottomNavigationView bottomNavigationView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_layout);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
         materialToolbar = findViewById(R.id.toolBar);
         bottomNavigationView = findViewById(R.id.bnMenu);
+        navigationView = findViewById(R.id.navDrawer);
 
         materialToolbar.setTitle("Café");
         getSupportFragmentManager()
@@ -71,5 +81,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, materialToolbar, R.string.navopen, R.string.navclose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
